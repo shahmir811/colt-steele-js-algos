59a95331ea2606b64e1752cee511584508239ea3
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-03-22 09:42:49 +0500

    Made separate folders for slidingWindow problems

--- exercises/01_slidingWindow/findingLongestSubstring/index.js
+++ exercises/01_slidingWindow/findingLongestSubstring/index.js
@@ -7,18 +7,18 @@
 
 const findLongestSubstring = str => {
 	let longest = 0;
-	let seen = {};
+	let obj = {};
 	let start = 0;
 
 	for (let i = 0; i < str.length; i++) {
 		let char = str[i];
-		if (seen[char]) {
-			start = Math.max(start, seen[char]);
+		if (obj[char]) {
+			start = Math.max(start, obj[char]);
 		}
 		// index - beginning of substring + 1 (to include current in count)
 		longest = Math.max(longest, i - start + 1);
 		// store the index of the next char so as to not double count
-		seen[char] = i + 1;
+		obj[char] = i + 1;
 	}
 	return longest;
 };
