fa4b0f9f45e2e2edd59d6c16cc6ee6ee25130a9f
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-06-01 10:33:01 +0500

    Updated 'reverse' method in SinglyLinkedList

--- exercises/05_dataStructures/02_singlyLinkedList/index.js
+++ exercises/05_dataStructures/02_singlyLinkedList/index.js
@@ -108,18 +108,25 @@ class SinglyLinkedList {
 		this.length--;
 		return removed;
 	}
+
+	// Following is the Andrei Neagoie Solution
 	reverse() {
-		let node = this.head;
-		this.head = this.tail;
-		this.tail = node;
-		let next;
-		let prev = null;
-		for (let i = 0; i < this.length; i++) {
-			next = node.next;
-			node.next = prev;
-			prev = node;
-			node = next;
+		if (!this.head.next) {
+			return this.head;
 		}
+		let first = this.head;
+		this.tail = this.head;
+		let second = first.next;
+
+		while (second) {
+			const temp = second.next;
+			second.next = first;
+			first = second;
+			second = temp;
+		}
+
+		this.head.next = null;
+		this.head = first;
 		return this;
 	}
 
